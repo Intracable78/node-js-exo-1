436ca72ae6baaddd31c446010bd7e28bdc4ed3ed
intracable78 <matteo.tomasi@ynov.com>
2022-05-21 18:25:06 +0200

    no sql

--- index.js
+++ index.js
@@ -4,71 +4,80 @@ const app = express();
 const port = 3000;
 app.use(express.json())
 
+const mongoose = require('mongoose');
+const client = mongoose.connect('mongodb://localhost:27017/databaseExo');
+const Book = mongoose.model('books', { name: String, quantity: String });
+
 //route test
 
 app.get('/', (req, res) => {
     res.send('Hello World, from express');
 });
 
-// data par defaut
-
-const books = [];
-books.push(
-    {
-        id: 1,
-        name: 'Premier Livre',
-        qty: '3'
-    },
-    {
-        id: 2,
-        name: 'Second Livre',
-        qty: '1'
-    }
-)
 
 // get all books
-app.get('/books', (req, res) => res.json(books));
+app.get('/books', async (req, res) => {
+    const allBooks = await Book.find();
+    res.send(allBooks);
+
+})
 
 // create one book
-app.post('/create/books', (req, res) => {
-    const book = req.body;
-    if (books.find(x => x.id === parseInt(req.body.id))) {
-        res.send('Sorry this id is already exists');
-        return;
-    }
-    books.push(book);
-    res.send('Book has been created successfully');
+app.post('/create/book', async (req, res) => {
+    const name = req.body.name;
+    const quantity = req.body.quantity;
+
+    const createBook = new Book({ name: name, quantity: quantity })
+    await createBook.save();
 })
+
 //get one book by id
-app.get('/book/:id', (req, res) => {
-    const searchBookByid = books.find(x => x.id === parseInt(req.params.id))
-    if (!searchBookByid) {
-        res.send('Error : book not exists')
-        return;
+app.get('/book/:id', async (req, res) => {
+    const bookId = req.params.id;
+    try {
+        const getBookById = await Book.findById(bookId)
+        res.send(getBookById);
+    } catch (err) {
+        res.send('book not found')
     }
-    res.send(searchBookByid);
 })
-//update one book by id
-app.post('/update/book/:id', (req, res) => {
-    const searchBookByid = books.find(x => x.id === req.params.id)
-    if (!searchBookByid) {
-        res.send('Error : book not exists')
-        return;
+
+app.get('/search/book/:name', async (req, res) => {
+    console.log("here")
+    const name = req.params.name
+    try {
+        const searchBook = await Book.find(({ name: { $regex: name } }));
+        res.send(searchBook)
+    } catch (err) {
+        res.send('book not found')
     }
-    searchBookByid.id = req.body.id;
-    searchBookByid.name = req.body.name;
-    searchBookByid.qty = req.body.qty;
-    res.send(searchBookByid);
+})
+
+//update one book by idÒ
+app.patch('/update/book/:id', async (req, res) => {
+    const bookId = req.params.id;
+    const name = req.body.name;
+    const quantity = req.body.quantity;
+    try {
+        const book = await Book.findById(bookId)
+        book.name = name;
+        book.quantity = quantity;
+        await book.save();
+        res.send(book);
+    } catch (err) {
+        res.send('book not found')
+    }
 })
 //delete one book by id
 
-app.delete('/delete/book/:id', (req, res) => {
-    const searchBookByid = books.findIndex(x => x.id === parseInt(req.params.id));
-    if (!books[searchBookByid]) {
-        res.send('Error : book not exists')
-        return;
+app.delete('/delete/book/:id', async (req, res) => {
+    const bookId = req.params.id;
+    try {
+        const book = await Book.delete(bookId)
+        await book.save();
+        res.send('Book has been deleted');
+    } catch (err) {
+        res.send('book not found')
     }
-    books.splice(searchBookByid, 1);
-    res.send('Book has been deleted successfully');
 })
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
